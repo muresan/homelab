b0daa72cc7a1f3be8f02e59f6eba7680ffe6cc96
Andrew Wyatt <andrew@fuduntu.org>
2018-08-04 13:44:35 -0400

    Fix a decom oops

--- cookbooks/enterprise_linux/recipes/decom.rb
+++ cookbooks/enterprise_linux/recipes/decom.rb
@@ -82,7 +82,7 @@ end
 
 execute 'Remove my DNS record' do
   command <<-EOF
-    curl -X GET "#{node['linux']['dns']['zonomi_url']}?host=$(hostname -f)&api_key=#{passwords['zonomi_api']}&action=DELETE 2>/dev/null”
+    curl -X GET "#{node['linux']['dns']['zonomi_url']}?host=$(hostname -f)&api_key=#{passwords['zonomi_api']}&action=DELETE" 2>/dev/null
   EOF
   action :run
   sensitive node['linux']['runtime']['sensitivity']
