fa8d93eb4e93ac76ed4560b6d351e5b2c3004030
Andrew Wyatt <andrew@fuduntu.org>
2018-08-08 07:05:39 -0400

    Enable forwarding for Rancher/Docker

--- cookbooks/rancher/recipes/rancher_server.rb
+++ cookbooks/rancher/recipes/rancher_server.rb
@@ -20,6 +20,8 @@
 node.default['linux']['firewall']['ports']['8080/tcp']    = true
 node.default['linux']['firewall']['ports']['9345/tcp']    = true
 node.default['linux']['firewall']['ignoreexisting']       = true
+node.default['linux']['sysctl']['net.ipv4.conf.all.forwarding'] = "1"
+
 
 ###
 ### Prerequisites
