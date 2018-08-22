a736f3fbe7c8874c3ae943fea7eae3dd83eb6b98
Andrew Wyatt <andrew@fuduntu.org>
2018-08-22 16:25:33 -0400

    Clean up wording of the Chef user creation notification.

--- cookbooks/chef/recipes/configure_users.rb
+++ cookbooks/chef/recipes/configure_users.rb
@@ -177,7 +177,7 @@ Subject: Chef server access (#{node['fqdn']}) (DO NOT REPLY)
 Content-Type: text/html
 MIME-Version: 1.0
 
-Hello #{attributes['firstname']} #{attributes['lastname']}!  You have been provisioned access to the #{node['chef']['runtime']['environment']} Chef server https://#{node['fqdn']}, servicing the #{node['chef']['runtime']['network']} network.  You have been generated a temporary password of '#{password}'.  Please log in and link your Chef account to your Active Directory account as soon as possible.  The system generated password will expire on first use.  Once logged into Chef, browse to Administration > Users, and reset your private key.  Save the private key to #{account}.pem when configuring knife for access to the Chef server.
+Hello #{attributes['firstname']} #{attributes['lastname']}!  You have been provisioned access to the #{node['chef']['runtime']['environment']} Chef server https://#{node['fqdn']}.  You have been generated a temporary password of '#{password}'.  Please log in and link your Chef account to your Active Directory account as soon as possible.  The system generated password will expire on first use.  Once logged into Chef, browse to Administration > Users, and reset your private key.  Save the private key to #{account}.pem when configuring knife for access to the Chef server.
 
 You can configure knife by cloning the knifectl project (https://github.com/andrewwyatt/knifecfg) on Github.
 
